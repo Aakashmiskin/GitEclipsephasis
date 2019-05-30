d0e0d42fec4eef926301a616829000310b39c2aa
Aakashmiskin <aakash.miskin@gmail.com>
2019-05-30 16:53:15 +0530

    Test employee

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,12 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Employee e1=new Employee();
+			e1.setEmpid(100);
+			e1.setEmpname("Aakash");
+			e1.setEmpsal(10000);
+	}
+
+}
